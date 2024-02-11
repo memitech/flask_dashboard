8ad7116df604eb3149371b551fe8a348b07a1839
memitech <info.angleinc@gmail.com>
2024-02-11 13:12:31 +0900

    edit template-engine

--- main.py
+++ main.py
@@ -5,10 +5,37 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    members = [
+        {
+        'name': 'Alexander Scholz',
+        'phone': '012-345-678',
+        'country': 'Denmark'
+        },
+        {
+        'name': 'Marius Hoibraten',
+        'phone': '123-456-789',
+        'country': 'Norway'
+        },
+        {
+        'name': 'Ekanit Panya',
+        'phone': '234-567-890',
+        'country': 'Thailand'
+        },
+    ]
+    events = [
+
+    ]
     return render_template(
-        'index.html'
+        'index.html',
+        members = members,
+        events = events,
     )
 
+@app.route('/form')
+def form():
+    return render_template(
+        'form.html'
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
